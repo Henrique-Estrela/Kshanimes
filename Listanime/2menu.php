96837b08c380cb09ed56e252b582e6075457f7e4
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-05-09 18:24:13 -0300

    Add files via upload

---
+++ Listanime/2menu.php
@@ -0,0 +1,33 @@
+<body>
+<img src="_images/banner3.png" id="banner-topo" class="img-fluid">
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <img src="_images/icone.png" id="imagem-topo" class="img-fluid">
+      
+    </a><h2>Listanime</h2>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="justify-content-end navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    
+    
+    
