3041091443870b0dbc5ae895b1ddc0dd69fd0639
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-05-13 18:02:12 -0300

    Add files via upload

---
+++ Listanime/3index.php
@@ -0,0 +1,21 @@
+<?php include "1header.php"; ?>
+<?php include "2menu.php"; ?>
+<?php include "banner.php"; ?>
+
+
+
+
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+  <h1>Lista Anime</h1>
+
+
+
+<?php include "4js.php"; ?>
+<?php include "5footer.php"; ?>
---
+++ Listanime/banner.php
@@ -0,0 +1,19 @@
+<div id="carouselExampleIndicators" class="carousel-fluid carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">
+      </button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="6000">
+        <img src="_images/banner1.jpg" id="imgBanner" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="6000">
+        <img src="_images/banner2.jpg" id="imgBanner" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="6000">
+        <img src="_images/banner.jpg" id="imgBanner" class="d-block w-100" alt="...">
+      </div>
+    </div>
+  </div>
