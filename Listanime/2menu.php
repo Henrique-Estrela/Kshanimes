e03a8adb954b2a52502d0ca538b2263e52b806d1
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-05-13 21:06:47 -0300

    Add files via upload

---
+++ Listanime/2menu.php
@@ -0,0 +1,49 @@
+<body>
+<img src="_images/banner3.png" id="banner-topo" class="img-fluid">
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="justify-content-start container-fluid">
+    <a class="navbar-brand" href="3index.php">
+    <img src="_images/logo.png" id="imagem-topo" class="img-fluid">
+      
+    </a>
+    <a class="nav-link" aria-current="page" id="logo_letra" href="3index.php"><h2><span id="L">L</span>istanime</h2></a>
+        
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- <div class="justify-content-end navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" id="logo_letra" href="rank.php">Rank</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="logo_letra" href="animes.php">Animes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="logo_letra" href="mangas.php">Mangas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="logo_letra" href="series.php">Series</a>
+        </li>
+      </ul>
+    </div> -->
+  </div>
+</nav>
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+      <a class="nav-link" id="logo_letra" href="rank.php">Rank</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="logo_letra" href="animes.php">Animes</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="logo_letra" href="mangas.php">Mangas</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="logo_letra" href="series.php">Series</a>
+    </li>
+</ul>
+
+    
+    
+    
